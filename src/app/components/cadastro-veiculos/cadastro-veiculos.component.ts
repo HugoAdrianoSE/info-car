0b3b45ca3d0f039d0fc3a73ede364fb8c6dee20e
Hugo Elias <hugo.elias@woozaventures.com.br>
2024-09-19 20:29:51 -0300

    Conexão API

--- src/app/components/cadastro-veiculos/cadastro-veiculos.component.ts
+++ src/app/components/cadastro-veiculos/cadastro-veiculos.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { CommonModule } from '@angular/common';
+import { ApiService } from '../../services/api.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-cadastro-veiculos',
@@ -16,9 +18,14 @@ import { CommonModule } from '@angular/common';
 
 export class CadastroVeiculosComponent {
   public showForm = false;
+  public listaVeiculos: any[] = [];
+
+  constructor(
+    private apiService: ApiService,
+  ) {}
 
   public async ngOnInit() {
-
+    this.getVeiculos();
   }
 
   public toggleForm() {
@@ -27,5 +34,16 @@ export class CadastroVeiculosComponent {
 
   public closeForm() {
     this.showForm = false;
+  }
+
+  private async getVeiculos() {
+    try {
+      const response = await firstValueFrom(this.apiService.getVeiculos());
+      if (response && response.result && response.result.lista) {
+        this.listaVeiculos = response.result.lista;
+      }
+    } catch (error) {
+      console.error('Erro ao carregar veículos:', error);
+    }
   }
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class ApiService {
+  private apiUrl = 'https://veiculos.free.beeceptor.com/api/veiculos';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  public getVeiculos(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+}
